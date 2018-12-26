c335cd1b0ef99f4eb607b7c31d30051efba6baa9
Rahul S. Deshpande <rahulsachindeshpande@gmail.com>
2018-12-25 20:49:11 -0800

    update color scheme + add pages

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to nav when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("nav");
-    if (x.className === "nav") {
+    if (x.className === "topnav") {
         x.className += " responsive";
     } else {
-        x.className = "nav";
+        x.className = "topnav";
     }
 }
