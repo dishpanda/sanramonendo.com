393cb9d4aa61922e1a0caf52f304a718ee8efdc5
Rahul S. Deshpande <rahulsachindeshpande@gmail.com>
2018-12-30 00:09:26 -0800

    website completely revamped to meet client expectations

--- main.js
+++
@@ -1,9 +0,0 @@
-/* Toggle between adding and removing the "responsive" class to nav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("nav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
