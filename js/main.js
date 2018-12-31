e08386a058ccac1d90c4ddc23e1742c98db74bbd
Rahul S. Deshpande <rahulsachindeshpande@gmail.com>
2018-12-31 01:49:16 -0800

    update site to 50% potential
    fix issue with address on mobile
    add patient education page
    add video on patient education page
    TODO:
    meet the team page
    contact us page
    refer-a-patient page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
+
+function toggleDrop(){
+  var x = document.getElementById("myDropdown");
+  if (x.className === "not-showing") {
+    x.className = "dropdown-content";
+    console.log("a");
+  } else {
+    x.className = "not-showing";
+    console.log("b");
+  }
+}
